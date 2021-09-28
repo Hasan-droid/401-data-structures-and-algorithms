749720e569e60ff26171afa3c9575392777f1a0e
hasan-droid <hasan-net2010@hotmail.com>
2021-09-29 00:45:59 +0300

    pair programming qestion 1 solutin

---
+++ CC34_(repated words with same amount) /HashTable.js
@@ -0,0 +1,66 @@
+'use strict'
+
+
+const LinkedList=require('./LinkedList');
+
+class HashTable{
+
+    constructor(size){
+        this.size=size;
+        this.map=new Array(size);
+
+    }
+
+    hash(key){
+        // console.log("splited key:" ,key.split('') )
+        return key.split('').reduce((acc , char)=>{
+            // console.log("char.charCodeAt(0)====>",char.charCodeAt(0))
+            return acc+char.charCodeAt(0);
+        } , 0)* 599 % this.size;
+    }
+
+    add(key , value){
+       let hash=this.hash(key);
+       if(!this.map[hash]){
+        console.log("hash>>>",hash)
+        this.map[hash]=new LinkedList();
+       }
+      
+       let entry={[key]:value};
+       console.log("{entry}" , entry)
+       this.map[hash].append(entry);
+       if(this.map[hash].head.next)
+       console.log("bucket2",this.map[hash].head.next.value)
+       console.log('bucket' , this.map[hash].head.value)
+      
+    }
+
+    get(key){
+        let hash=this.hash(key);
+        if(!this.map[hash])
+        return false
+       
+        
+        let temp=this.map[hash].head;
+        let value=temp.value.name;
+              while(temp.next){
+            value+="\n"+temp.next.value.name;
+            temp=temp.next;
+        }
+      console.log("value from get" , value)
+        return value;
+    }
+
+    contains(key){
+
+        let hash=this.hash(key);
+        if(!this.map[hash])
+        return false
+       return true;
+
+    }
+    
+
+}
+
+module.exports=HashTable;
---
+++ CC34_(repated words with same amount) /readme.md
@@ -0,0 +1,11 @@
+# hash table
+
+## pair programming question 1 (repated words same amount and catch first occurence )
+
+## white board
+
+![white board](repated-words-same=amount-hash-whiteboard.png)
+
+## tests
+
+![test](repated-words-same=amount-hash-test-results.PNG)
